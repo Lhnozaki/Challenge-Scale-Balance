92c96e05c90d3818e8e192a34caaa8cf586581f0
Lewis Nozaki <lhnozaki@gmail.com>
2019-10-01 22:23:29 -1000

    created and passed tests

--- index.js
+++ index.js
@@ -1,27 +1,66 @@
 function scaleBalance(strArr) {
-  let regex = /[\[\],]+/g;
-  let scale = strArr[0]
-    .replace(regex, "")
-    .split(" ")
-    .map(e => parseInt(e));
-  let values = strArr[1]
-    .replace(regex, "")
-    .split(" ")
-    .map(e => parseInt(e))
-    .filter(e => !Number.isNaN(e));
-  let resultsArr = [];
+  let scale = JSON.parse(strArr[0]);
+  let values = JSON.parse(strArr[1]);
+  let left = scale[0];
+  let right = scale[1];
+  let difference = Math.floor(Math.abs(left - right));
+  let returnValue = "Not Possible";
+  let addLeft = values.map(e => {
+    return e + left;
+  });
 
-  // Check if equal at start
-  if (scale[0] === scale[1]) {
-    return "Not Possible";
-  }
+  let addRight = values.map(e => {
+    return e + right;
+  });
 
   // Check if values array is empty
   if (values.length === 0) {
-    return "No values";
+    returnValue = "No values";
   }
 
-  return strArr.join("");
+  // Check if equal at start
+  if (left === right) {
+    returnValue = "Not Possible";
+  } else {
+    // Balance scale
+    addLeft.forEach(e => {
+      addRight.forEach(f => {
+        if (e === f) {
+          if (addLeft.indexOf(e) > addRight.indexOf(f)) {
+            returnValue = `${values[addRight.indexOf(f)]}, ${
+              values[addLeft.indexOf(e)]
+            }`;
+          } else {
+            returnValue = `${values[addLeft.indexOf(e)]}, ${
+              values[addRight.indexOf(f)]
+            }`;
+          }
+        }
+      });
+    });
+  }
+
+  // Check if difference between left and right are in the list of values
+  values.forEach(e => {
+    if (e === difference) {
+      returnValue = `0, ${e}`;
+    }
+  });
+
+  let arr = [];
+  values.forEach(e => {
+    if (e < difference) {
+      arr.push(e);
+      difference -= e;
+    } else {
+      if (e === difference) {
+        arr.push(e);
+        difference -= e;
+      }
+    }
+  });
+
+  return returnValue;
 }
 
 module.exports = scaleBalance;
